4c273273b9adeca857370f31e7ff1ea831f953cb
facundo aquino <facun90@live.com>
2024-03-04 20:31:44 -0300

    feat: Add products components

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../material/material.module';
+import { ProductsComponent } from '../products/products.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, ProductsComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
---
+++ src/app/products/product-card/product-card.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MaterialModule } from '../../material/material.module';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [MaterialModule, ReactiveFormsModule, CommonModule
+],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss'
+})
+export class ProductCardComponent implements OnInit{
+  @Input() product: any;
+    quantityControl = new FormControl(1);
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addToCart() {
+    console.log('Agregar al carrito', this.quantityControl);
+  }
+
+  increaseQuantity() {
+    this.quantityControl.setValue(this.quantityControl.value! + 1);
+  }
+
+  decreaseQuantity() {
+    if (this.quantityControl.value! > 0) {
+      this.quantityControl.setValue(this.quantityControl.value! - 1);
+    }
+  }
+}
